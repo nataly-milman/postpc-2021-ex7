832c2900f0f8d35d6479ff0b4c423ef07cc58592
Nataly Milman <n.pervusiak@gmail.com>
2021-06-09 22:42:29 +0300

    initial commit

---
+++ app/src/main/java/exercise/android/nami/sandwiches/MainActivity.java
@@ -0,0 +1,14 @@
+package exercise.android.nami.sandwiches;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
